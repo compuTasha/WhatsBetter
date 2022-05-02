ceb827349f32287490f292134f2b022270d4ae1a
compuTasha <mijookim.tasha@gmail.com>
2022-05-02 09:46:35 +0900

    Initial Commit

---
+++ WhatsBetter/WhatsBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsBetterApp.swift
+//  WhatsBetter
+//
+//  Created by Mijoo Kim on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct WhatsBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
