7139868303896327fc8ad0ed8b727e1cabb40d3e
compuTasha <mijookim.tasha@gmail.com>
2022-05-05 16:16:33 +0900

    💄 Post layout 수정 (#1)

---
+++ WhatsBetter/Global/Extension/Color+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  Color+Extension.swift
+//  WhatsBetter
+//
+//  Created by Mijoo Kim on 2022/05/04.
+//
+
+import SwiftUI
+
+extension Color {
+    static var hashtagGrey: Color {
+        return Color(hex: "EFF0F0")
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+        
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0)
+    }
+}
